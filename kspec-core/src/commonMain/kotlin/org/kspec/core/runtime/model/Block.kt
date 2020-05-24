61ca3343ca0ccf70bc72a747dc3fe9dd7b581bf7
Mori Atsushi <at.sushi.tb@gmail.com>
2020-05-25 02:58:08 +0900

    Add block class

---
+++ kspec-core/src/commonMain/kotlin/org/kspec/core/runtime/model/Block.kt
@@ -0,0 +1,15 @@
+package org.kspec.core.runtime.model
+
+import org.kspec.core.dsl.body.TestBody
+
+internal sealed class Block {
+    data class Group(
+        val name: Name,
+        val blocks: List<Block>
+    ) : Block()
+
+    data class Test(
+        val name: Name?,
+        val body: TestBody.() -> Unit
+    ) : Block()
+}
