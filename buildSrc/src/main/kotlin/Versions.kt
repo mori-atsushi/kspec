8fb930c0d1f4c72a66000aeb93d3caaf6e11cb6d
Mori Atsushi <at.sushi.tb@gmail.com>
2020-05-25 07:45:21 +0900

    Setup build.gradle for multiplatform

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,3 +1,3 @@
 object Versions {
-    val kotlin = "1.3.72"
+    const val kotlin = "1.3.72"
 }
--- kspec-core/build.gradle.kts
+++ kspec-core/build.gradle.kts
@@ -3,10 +3,34 @@ plugins {
 }
 
 kotlin {
+    jvm()
+    linuxX64("linux")
+    macosX64("macos")
+    mingwX64("windows")
+
     sourceSets {
-        commonMain {
+        val commonMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-common"))
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        jvm().compilations["main"].defaultSourceSet {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+            }
+        }
+
+        jvm().compilations["test"].defaultSourceSet {
+            dependencies {
+                implementation(kotlin("test-junit"))
             }
         }
     }
